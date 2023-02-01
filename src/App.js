5eee2682e3328a422ab7bbdcbbd0a9768f8ed916
Chamas111 <ahmad.chamas87@gmail.com>
2023-02-01 17:19:29 +0100

    trying to display data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import "./App.css";
-import ListOfNews from "./ListOfNews";
-import Nav from "./Nav";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [posts, setPost] = useState([]);
+  const [searchInput, setSeachInput] = useState("");
+
+  const renderNews = (data) => {
+    const news = data.hits;
+    console.log({ news });
+    return news.map((newsItem) => {
+      return <div>newsItem.title</div>;
+    });
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://hn.algolia.com/api/v1/search_by_date?query=openai&tags=story"
+    )
+      .then((response) => response.json())
+      .then((response) => renderNews(response));
+  }, []);
+
+  /*   axios
+      .get(
+        "https://hn.algolia.com/api/v1/search_by_date?query=openai&tags=story"
+      )
+      .then((res).json() => {
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+ */
   return (
     <div className="App">
-      <Nav />
-      <ListOfNews />
-      <h1>Hello</h1>
+      <nav className="navbar bg-primary" data-bs-theme="dark">
+        <div className="container">
+          <a className="navbar-brand">Logo</a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={(e) => setSeachInput(e.target.value)}
+            />
+            <button className="btn btn-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <div className="container">
+        {posts.map((post) => (
+          <>
+            <h3 key={post.id}>{post.hits[0]}</h3>
+            <p>{post.body}</p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
