8359f0e4367689c6356827a6db588d654ff43c45
Chamas111 <ahmad.chamas87@gmail.com>
2023-02-01 23:16:31 +0100

    Display and little bit worked with Search

--- src/App.js
+++ src/App.js
@@ -1,68 +1,55 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-
+import Nav from "./Nav";
 import axios from "axios";
 
 function App() {
   const [posts, setPost] = useState([]);
-  const [searchInput, setSeachInput] = useState("");
-
-  const renderNews = (data) => {
-    const news = data.hits;
-    console.log({ news });
-    return news.map((newsItem) => {
-      return <div>newsItem.title</div>;
-    });
-  };
-
+  const [searchInput, setSeachInput] = useState([]);
+  var d = new Date();
   useEffect(() => {
-    fetch(
-      "https://hn.algolia.com/api/v1/search_by_date?query=openai&tags=story"
-    )
-      .then((response) => response.json())
-      .then((response) => renderNews(response));
-  }, []);
-
-  /*   axios
+    axios
       .get(
         "https://hn.algolia.com/api/v1/search_by_date?query=openai&tags=story"
       )
-      .then((res).json() => {
-        setPost(res.data);
+      .then((res) => {
+        setPost(res.data.hits);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
- */
+
   return (
-    <div className="App">
-      <nav className="navbar bg-primary" data-bs-theme="dark">
-        <div className="container">
-          <a className="navbar-brand">Logo</a>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              onChange={(e) => setSeachInput(e.target.value)}
-            />
-            <button className="btn btn-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
-      <div className="container">
-        {posts.map((post) => (
-          <>
-            <h3 key={post.id}>{post.hits[0]}</h3>
-            <p>{post.body}</p>
-          </>
-        ))}
+    <>
+      <div className="App">
+        <Nav posts={posts} setSeachInput={setSeachInput} />
+
+        <article className="container mt-3">
+          <ol>
+            {posts.map(
+              ({ title, url, points, num_comments, author, objectID }) => (
+                <div>
+                  <h5>
+                    <li key={objectID}>
+                      <a href={url} target="_blank">
+                        {title}.
+                      </a>
+                    </li>
+                  </h5>
+
+                  <p>
+                    {points} points by {author} {d.getHours()} hours ago | Hide
+                    | comments: {num_comments}
+                  </p>
+                  <hr />
+                </div>
+              )
+            )}
+          </ol>
+        </article>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,32 @@
 import React from "react";
 
-function Nav() {
+function Nav({ posts, setSeachInput }) {
+  const handleSubmit = (e) => e.preventDefault();
+
+  const handleSearchChange = (e) => {
+    if (!e.target.value) return setSeachInput(posts);
+    const res = posts.filter((item) => item.title.includes(e.target.value));
+
+    setSeachInput(res);
+  };
   return (
     <div>
-      <nav class="navbar bg-primary" data-bs-theme="dark">
-        <div class="container-fluid">
-          <a class="navbar-brand">Logo</a>
-          <form class="d-flex" role="search">
+      <nav className="navbar bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand">Logo</a>
+          <form className="d-flex" role="search">
             <input
-              class="form-control me-2"
+              className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
+              onChange={handleSearchChange}
             />
-            <button class="btn btn-success" type="submit">
+            <button
+              className="btn btn-success"
+              type="submit"
+              onClick={handleSubmit}
+            >
               Search
             </button>
           </form>
